d354092fa71c43ebe51b9fd9817606ef0063328d
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-04-13 10:40:27 -0600

    add class of active to season and colors

--- src/components/Flowers.js
+++ src/components/Flowers.js
@@ -11,9 +11,9 @@ export default class Flowers extends Component {
       <div className="flowers-div">
       <h2>{data.season} Wedding Floral Palette</h2>
         <div className="color-palette">
-          {data.colors.map((color, i) => <div key={i} style={{backgroundColor: data.colors[i]}}>{ color }</div>)}
+          {data.colors.map((color, i) => <div key={i}><div style={{backgroundColor: data.colors[i]}}></div><p>{color}</p></div>)}
         </div>
-        {flowers.map((flower, i) => (flower.colors.includes(data.colors[0] || data.colors[1] || data.colors[2] ) && (flower.seasons.includes(data.season)))? <div className="indv-flower"key={i}><p>{flower.flower_name}</p> <p>{flower.seasons}</p><p>{flower.colors}</p><img src={flower.image}/></div>: null)}
+        {flowers.map((flower, i) => (flower.colors.includes(data.colors[0] || data.colors[1] || data.colors[2] ) && (flower.seasons.includes(data.season)))? <div className="indv-flower"key={i}><p className="flower-name"> {flower.flower_name}</p> <p>Seasons: {flower.seasons}</p><p>Colors: {flower.colors}</p><img src={flower.image} alt={flower.flower_name}/></div>: null)}
       </div>
     )
   }
