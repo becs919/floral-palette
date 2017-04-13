c6df7f836693df7500387ba385c279be2b85bdfb
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-04-12 18:47:16 -0600

    cannot add double colors into state. flowers display names matching colors

--- src/components/Flowers.js
+++ src/components/Flowers.js
@@ -7,19 +7,14 @@ export default class Flowers extends Component {
   render(){
     const { data, flowers } = this.props;
 
-    const divStyle = {
-      backgroundColor: `${data.colors}`
-    };
-
     return (
       <div className="flowers-div">
       <h2>{data.season} Wedding Floral Palette</h2>
         <div className="color-palette">
           {data.colors.map((color, i) => <div key={i} style={{backgroundColor: data.colors[i]}}>{ color }</div>)}
         </div>
-        // {flowers.map((flower, i) => console.log(flower.name))}
+        {flowers.map((flower, i) => flower.colors.includes(data.colors[0] || data.colors[1] || data.colors[2] )? <p key={i}>{flower.flower_name}</p>: null)}
       </div>
     )
   }
 }
-// <p key={i}>{flower.name}</p>
